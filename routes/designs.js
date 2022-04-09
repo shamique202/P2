db425b80887cc2f0c4ec62d81754d5204f925217
shamique202 <shamique.e@gmail.com>
2022-04-09 19:49:45 -0400

    .create

--- routes/designs.js
+++ routes/designs.js
@@ -3,3 +3,6 @@ const router = express.Router();
 const designsCtrl = require('../controllers/designs');
 const loggedIn = require('../config/auth');
 
+router.get('/details/:id/designs/new', loggedIn, designsCtrl.new);
+router.get('/designs/:id', loggedIn, designsCtrl.show);
+router.post('/details/:id/designs', designsCtrl.create);
