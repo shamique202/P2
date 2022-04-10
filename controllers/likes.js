274461364d8dfe091ba4e10433bff44d513c323c
shamique202 <shamique.e@gmail.com>
2022-04-09 21:28:16 -0400

    delete likes

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,1 +1,15 @@
-const designs = require('../models/design');
+const Designs = require('../models/design');
+
+module.exports = {
+    edit,
+    create,
+    delete: deleteLikes,
+    update,
+};
+
+function edit(req, res) {
+    Designs.findOne({ 'likes._id': req.params.id }, function (err, design) {
+        res.render('likes/edit', { title: 'Edit Your Comment', design, like: req.params.id });
+    });
+};
+
