2830f073ba074ce3584db51887552f50b2067978
shamique202 <shamique.e@gmail.com>
2022-04-10 17:53:04 -0400

    user schemas created

--- models/user.js
+++ models/user.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+const userSchema = new Schema({
+    name: String,
+    googleId: {
+        type: String,
+        required: true
+    },
+    email: String,
+    avatar: String,
+    admin: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
