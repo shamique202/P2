39f8e54cf95743a6c9de8dc113c4dea6fedec5f4
shamique202 <shamique.e@gmail.com>
2022-04-09 22:30:07 -0400

    module.exports

--- controllers/details.js
+++ controllers/details.js
@@ -1,2 +1,11 @@
 const Details = require('../models/details');
 const Designs = require('../models/design');
+
+module.exports = {
+
+    index,
+    new: newDetail,
+    show,
+    create,
+    delete: deleteDetails,
+};
