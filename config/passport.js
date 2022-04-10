6cf07ccb2da830552ac27a42a5b0caa00a300941
shamique202 <shamique.e@gmail.com>
2022-04-10 17:38:35 -0400

    finish passport

--- config/passport.js
+++ config/passport.js
@@ -1,30 +1,47 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-//Require your User Model here!
+const User = require('../models/user');   //Require your User Model here!
 
-// configuring Passport!
+// configuring Passport
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
+},
+  // callback function
+  function (accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
     // refer to the lesson plan from earlier today in order to set this up
-
+    User.findOne({ googleId: profile.id }).then(async function (user) {
+      if (user) return cb(null, user);
+      // new user on the OAuth
+      try {
+        user = await User.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value,
+          admin: false,
+        });
+        return cb(null, user);
+      } catch (err) {
+        cb(err);
+      }
+    });
   }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+)
+);
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-
-  // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
+passport.deserializeUser(function (id, done) {
+  // finding the user, then calling cb
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
-
+  User.findById(userId).then(function (user) {
+    cb(null, user);
+  });
 });
 
 
