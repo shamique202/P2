c63014478be7d3d18dae8f900ddd6192d59adc55
shamique202 <shamique.e@gmail.com>
2022-04-09 21:45:34 -0400

    for likes

--- controllers/likes.js
+++ controllers/likes.js
@@ -9,7 +9,23 @@ module.exports = {
 
 function edit(req, res) {
     Designs.findOne({ 'likes._id': req.params.id }, function (err, design) {
-        res.render('likes/edit', { title: 'Edit Your Comment', design, like: req.params.id });
+        res.render('likes/edit', { title: 'Edit comment here', design, like: req.params.id });
     });
 };
+
+function create(req, res) {
+    Designs.findById(req.params.id, function (err, design) {
+        // add user's info 
+        req.body.user = req.user._id;
+        req.body.userName = req.user.name;
+
+        design.likes.push(req.body);
+        design.save(function (err) {
+            res.redirect(`/designs/${design._id}`);
+        });
+    });
+};
+
+
+
 
