f66fbd49dab4a3652b3109139a8f0df559bd51c5
shamique202 <shamique.e@gmail.com>
2022-04-10 17:18:18 -0400

    designs

--- controllers/designs.js
+++ controllers/designs.js
@@ -8,3 +8,33 @@ module.exports = {
     delete: deleteDesign,
 };
 
+function newDesign(req, res) {
+    Designs.findById(req.params.id, function (err, designs) {
+        res.render('designs/new', { title: 'Add Design Concept', designs, id: req.params.id });
+    })
+}
+
+function show(req, res) {
+    Designs.findById(req.params.id, function (err, design) {
+        Details.findById(design.detail, function (err, detail) {
+            res.render('designs/show', { title: 'The details of home decor', design, detail });
+        });
+    });
+};
+
+function create(req, res) {
+    let i = req.body.artists;
+    let d = req.body.informations;
+    req.body.artists = i.split('\r\n');
+    req.body.informations = d.split('\r\n');
+    req.body.details = req.params.id
+    Designs.create(req.body, function (err) {
+        res.redirect(`/details/${req.params.id}`);
+    });
+};
+
+function deleteDesign(req, res) {
+    Designs.findByIdAndDelete(req.params.id, function (err, deletedDesign) {
+        res.redirect(`/details`);
+    });
+};
