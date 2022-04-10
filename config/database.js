35646186455ed461e693e3f21061595673fe7055
shamique202 <shamique.e@gmail.com>
2022-04-10 17:45:18 -0400

    mongoDB connection

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-// replace your database connection string here
-mongoose.connect(process.env.DATABASE_URL,{ 
+// database connection string here
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
+//mongoose.connection obj.
 const db = mongoose.connection;
 
 // database connection event
 db.on('connected', function () {
-  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
+  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
