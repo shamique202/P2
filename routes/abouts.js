555b22b40672e2ed9af861c452ef2296f134a3c5
shamique202 <shamique.e@gmail.com>
2022-04-09 20:32:09 -0400

    about me section

--- routes/abouts.js
+++ routes/abouts.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const aboutsCtrl = require('../controllers/abouts');
+
+router.get('/', aboutsCtrl.index);
+
+module.exports = router;
